ddf2f85d16f8e36515ea9b937e70fb5728bd71aa
germanp <germanp@fc40e148-fcf8-41c6-9afc-3bac1f283b09>
2010-06-01 23:16:19 +0000

    Se agregó la carpeta modelo/ y las plantillas para construir las clases Se actualizó el diagrama de clases y se agregó la versión .svg

---
+++ modelo/plantilla.cpp
@@ -0,0 +1,15 @@
+/**
+ * @file   gorilla.cpp
+ * @author german <german@yahoo.com.ar>
+ * @date   Tue Jun  1 19:59:58 2010
+ *
+ * @brief <Descripción del archivo>
+ *
+ * @section TODO
+ * - Cosa que no tengo ganas de hacer ahora 1.
+ * - Cosa que no tengo ganas de hacer ahora 2.
+ */
+
+int Gorilla::darBanana(int bananas){
+  return lineasCodXBanana*bananas;
+}
---
+++ modelo/plantilla.h
@@ -0,0 +1,42 @@
+#ifndef PLANTILLA_H
+#define PLANTILLA_H
+
+/*      ^
+        |
+   Poné siempre esto al principio de los .h
+   Pero cambiale PLANTILLA_H por el nombre del archivo */
+
+/* Y también poné esto de abajo, pero cambiale lo que hay que
+   cambiar no seas gorila */
+
+/**
+ * @file   plantilla.h
+ * @author german <german@yahoo.com.ar>
+ * @date   Tue Jun  1 19:14:58 2010
+ *
+ * @brief <Descripción de lo que hace la clase>
+ *
+ * @section TODO
+ *  <Cosas que se te ocurrieron hacer y no tenés tiempo>
+ */
+
+
+class Gorilla {
+ private:
+  // Cant. de líneas de código tiradas por banana obtenida.
+  int lineasCodXBanana;
+ public:
+  /**
+   * Le dá mas bananas al gorilla para que
+   * tire más código
+   *
+   * @param bananas Cantidad de bananas que
+   * le damos al gorilla.
+   *
+   * @return Devuelve la cantidad de líneas
+   * de código que va a tirar el gorilla.
+   */
+  int darBananas(int bananas);
+};
+
+#endif // PLANTILLA_H
